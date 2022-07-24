9124ef82eef96eb8b040e67d97fb188657b1d8db
noelm <noel@noelmrtn.fr>
2022-07-24 19:14:48 +0200

    event loop

--- examples/chat.py
+++ examples/chat.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../')
 import vydeotel as vy
+import asyncio
 
 minitel = vy.Minitel("/dev/ttyS0")
 
@@ -17,3 +18,25 @@ minitel.println("Mot de passe  ...............")
 
 minitel.move_cursor_xy(15,3)
 minitel.cursor()
+
+async def keyboard_event(minitel: vy.Minitel):
+    buffer = ""
+    while True:
+        key = minitel.get_key_code()
+        if key == vy.CONNEXION_FIN:
+            print("CONNEXION FIN")
+        elif key == vy.RETOUR:
+            print("RETOUR")
+        elif key == vy.GUIDE:
+            print("GUIDE")
+        elif input == vy.ENVOI:
+            print("ENVOI")
+            print(buffer)
+            buffer = ""
+        elif input == vy.SOMMAIRE:
+            print("SOMMAIRE")
+        else:
+            input_buffer += chr(input)
+
+asyncio.get_event_loop().run_until_complete(keyboard_event(minitel))
+asyncio.get_event_loop().run_forever()
