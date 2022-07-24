d98fac9cd3c34c2678b2d010004b22ecd23653c8
noelm <noel@noelmrtn.fr>
2022-07-24 18:55:17 +0200

    setup menu

--- examples/chat.py
+++ examples/chat.py
@@ -5,10 +5,15 @@ import vydeotel as vy
 minitel = vy.Minitel("/dev/ttyS0")
 
 minitel.clean_screen()
+minitel.move_cursor_xy(0,0)
 
+minitel.print("")
 minitel.set_attribute(vy.DOUBLE_GRANDEUR)
 minitel.println("Messagerie")
 minitel.set_attribute(vy.GRANDEUR_NORMALE)
 
 minitel.println("Pseudo        ...............")
 minitel.println("Mot de passe  ...............")
+
+minitel.move_cursor_xy(15,3)
+minitel.cursor()
