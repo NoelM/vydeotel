8b24a8fc06fae9b2cb54295466998004cdbc28b8
noelm <noel@noelmrtn.fr>
2022-07-29 10:29:51 +0200

    countdown

--- examples/demo.py
+++ examples/demo.py
@@ -8,10 +8,10 @@ import vydeotel as vy
 def splash(mntl: vy.Minitel, title: str, subtitle: str):
     mntl.clean_screen()
     mntl.set_attribute(vy.FIXE)
-    mntl.move_cursor_xy(2, 5)
+    mntl.move_cursor_xy(10, 5)
     mntl.set_attribute(vy.DOUBLE_GRANDEUR)
     mntl.println(title.upper())
-    mntl.move_cursor_xy(2, 8)
+    mntl.move_cursor_xy(10, 8)
     mntl.set_attribute(vy.DOUBLE_HAUTEUR)
     mntl.set_attribute(vy.CLIGNOTEMENT)
     mntl.println(subtitle.upper())
@@ -20,16 +20,18 @@ def splash(mntl: vy.Minitel, title: str, subtitle: str):
 def count_down_screen(mntl: vy.Minitel, squad: str, speaker: str, subtitle: str, duration: int):
     mntl.clean_screen()
     mntl.set_attribute(vy.FIXE)
-    mntl.move_cursor_xy(2, 2)
+    mntl.move_cursor_xy(5, 3)
     mntl.set_attribute(vy.DOUBLE_HAUTEUR)
     mntl.set_attribute(vy.INVERSION_FOND)
     mntl.println(f"{squad.upper()}")
-    mntl.move_cursor_xy(2, 6)
+
+    mntl.move_cursor_xy(5, 7)
     mntl.set_attribute(vy.DOUBLE_GRANDEUR)
     mntl.set_attribute(vy.FOND_NORMAL)
     mntl.set_attribute(vy.CARACTERE_BLANC)
     mntl.println(f"{speaker.upper()}")
-    mntl.move_cursor_xy(2, 7)
+    mntl.move_cursor_xy(5, 8)
+
     mntl.set_attribute(vy.GRANDEUR_NORMALE)
     mntl.println(subtitle)
 
@@ -39,12 +41,14 @@ def count_down_screen(mntl: vy.Minitel, squad: str, speaker: str, subtitle: str,
 def count_down(mntl: vy.Minitel, duration: int):
     mntl.set_attribute(vy.DOUBLE_GRANDEUR)
     for remaining in reversed(range(duration)):
-        mntl.move_cursor_xy(5, 10)
+        mntl.move_cursor_xy(10, 20)
         mntl.clear_line()
-        mntl.move_cursor_xy(5, 11)
+        mntl.move_cursor_xy(10, 19)
         mntl.clear_line()
-        mntl.move_cursor_xy(5, 10)
-        mntl.print(f"{remaining}")
+        mntl.move_cursor_xy(10, 20)
+
+        m, s = divmod(remaining, 60)
+        mntl.print("{:02d}:{:02d}".format(m, s))
         time.sleep(1)
 
 
@@ -54,6 +58,6 @@ splash(minitel, "DEMO", "SPRINT 22.15")
 time.sleep(5)
 splash(minitel, "SQUAD BLUE", "3 ORATEURS")
 time.sleep(5)
-count_down_screen(minitel, "BLUE", "TOTO", "TITI", 60)
+count_down_screen(minitel, "SQUAD BLUE", "Jean Michel", "Des filtres", 90)
 
 
