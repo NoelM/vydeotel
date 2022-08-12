1e631a48ea46861ca467cad8956a9c84e0e0150d
noelm <noel@noelmrtn.fr>
2022-08-12 12:51:27 +0200

    wip: demo

--- examples/demo.py
+++ examples/demo.py
@@ -12,7 +12,7 @@ def splash(mntl: vy.Minitel, title: str, subtitle: str):
     display_vdt(mntl, "3615demo.vdt")
 
     mntl.text_mode()
-    mntl.move_cursor_xy(10, 18)
+    mntl.move_cursor_xy(8, 19)
     mntl.set_attribute(vy.CARACTERE_BLANC)
     mntl.set_attribute(vy.FOND_NORMAL)
     mntl.set_attribute(vy.DOUBLE_GRANDEUR)
